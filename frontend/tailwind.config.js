94e02c9617adee23983b5362e55ad881f7791194
Maruf <marufibnhossain@gmail.com>
2024-07-28 19:27:41 +0600

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
+    screens: {
+      '4xs': '270px',
+      // => @media (min-width: 270px) { ... }
+
+      '3xs': '360px',
+      // => @media (min-width: 360px) { ... }
+
+      '2xs': '414px',
+      // => @media (min-width: 414px) { ... }
+
+      'mxs': '480px',
+      // => @media (min-width: 480px) { ... }
+
+      'xs': '576px',
+      // => @media (min-width: 576px) { ... }
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+    require('flowbite/plugin'),
+    require("tw-elements/dist/plugin.cjs"),
+    // require("daisyui")
+  ],
+  darkMode: "class"
+}
