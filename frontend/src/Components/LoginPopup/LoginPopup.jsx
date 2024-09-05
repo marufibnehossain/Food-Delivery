c101820d1bba3965b4fe234ae4ca08ad6ce29056
Maruf <marufibnhossain@gmail.com>
2024-09-06 00:12:21 +0600

    authentication

--- frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -1,21 +1,58 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../../assets/frontend_assets/assets';
+import { StoreContext } from '../../Context/StoreContext';
+import axios from 'axios'
 
 const LoginPopup = ({setShowLogin}) => {
-  const [currState, setCurrState] = useState("Sign Up");
+  const {url, setToken} = useContext(StoreContext)
+  const [currState, setCurrState] = useState("Login");
+  const [data, setData] = useState({
+    name:"",
+    email:"",
+    password:"",
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(prevData=>({...prevData, [name]:value}))
+  }
+
+  const onLogin = async (event) => {
+    event.preventDefault()
+    let newUrl = url;
+    if (currState === "login") {
+      newUrl+='/api/user/login'
+    }
+    else{
+      newUrl += '/api/user/register'
+    }
+
+    const response = await axios.post(newUrl, data);
+
+    if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      setShowLogin(false)
+    }
+    else{
+      alert(response.data.message)
+    }
+  }
+
   return (
     <div className='login-popup absolute z-[100] w-full h-full bg-black bg-opacity-40 grid'>
-      <form className="login-popup-container place-self-center w-[max(23vw,330px)] text-[#808080] bg-white flex flex-col py-6 px-[30px] rounded-lg text-sm animate-[fadeIn_0.5s_ease-in-out] gap-8">
+      <form onSubmit={onLogin} className="login-popup-container place-self-center w-[max(23vw,330px)] text-[#808080] bg-white flex flex-col py-6 px-[30px] rounded-lg text-sm animate-[fadeIn_0.5s_ease-in-out] gap-8">
         <div className="login-popup-title flex justify-between items-center text-black">
           <h2 className='text-3xl font-bold'>{currState}</h2>
           <img className='w-4 cursor-pointer' onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className="login-popup-inputs flex flex-col gap-4">
-          {currState==="Login"?<></>:<input className='outline-none border-[1px_solid_#c9c9c9] p-[10px] rounded' type="text" placeholder='Your Name' name="" id="" required />}
-          <input className='outline-none border-[1px_solid_#c9c9c9] p-[10px] rounded' type="email" placeholder='Your Email' name="" id="" required />
-          <input className='outline-none border-[1px_solid_#c9c9c9] p-[10px] rounded' type="password" placeholder='Password' name="" id="" required />
+          {currState==="Login"?<></>:<input className='outline-none border-[1px_solid_#c9c9c9] p-[10px] rounded' type="text" placeholder='Your Name' name="name" onChange={onChangeHandler} value={data.name} id="" required />}
+          <input className='outline-none border-[1px_solid_#c9c9c9] p-[10px] rounded' type="email" placeholder='Your Email' name="email" onChange={onChangeHandler} value={data.email} id="" required />
+          <input className='outline-none border-[1px_solid_#c9c9c9] p-[10px] rounded' type="password" placeholder='Password' name="password" onChange={onChangeHandler} value={data.password} id="" required />
         </div>
-        <button className='border-none p-[10px] rounded text-white bg-[#ff6347] text-[15px] cursor-pointer'>{currState==="Sign Up"?"Create Account":"Login"}</button>
+        <button type='submit' className='border-none p-[10px] rounded text-white bg-[#ff6347] text-[15px] cursor-pointer'>{currState==="Sign Up"?"Create Account":"Login"}</button>
         <div>
           <div className="login-popup-condition flex items-center gap-2 -mt-5">
             <input className='se' type="checkbox" required />
--- frontend/src/Components/Shared/Navbar.jsx
+++ frontend/src/Components/Shared/Navbar.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect, useContext } from "react";
 import { assets } from "../../assets/frontend_assets/assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ScrollToId from "../ScrollToId/ScrollToId";
 import { StoreContext } from "../../Context/StoreContext";
 
 const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
   const [scrolled, setScrolled] = useState(false);
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const {getTotalCartAmount, token, setToken} = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/")
+  }
 
   useEffect(() => {
     const handleScroll = () => {
@@ -36,12 +43,13 @@ const Navbar = ({setShowLogin}) => {
           <Link to={'/'}><img className="lg:w-40 md:w-32 w-28" src={assets.logo} alt="" /></Link>
           <ul className="nav-menu lg:flex list-none lg:gap-5 text-[#49557e] lg:text-lg hidden ">
             {[
-              ["Home", "home", "/"],
-              ["Menu", "menu", "#explore-menu"],
-              ["Mobile App", "mobile-app", "#app-download"],
-              ["Contact Us", "contact-us", "#footer"],
-            ].map(([title, menuKey, link]) => (
+              ["1","Home", "home", "/"],
+              ["2","Menu", "menu", "#explore-menu"],
+              ["3","Mobile App", "mobile-app", "#app-download"],
+              ["4","Contact Us", "contact-us", "#footer"],
+            ].map(([id,title, menuKey, link]) => (
               <Link
+                key={id}
                 to={link}
                 onClick={() => setMenu(menuKey)}
                 className={`${menu === menuKey ? "active" : ""} cursor-pointer`}
@@ -56,9 +64,17 @@ const Navbar = ({setShowLogin}) => {
               <Link to={'/cart'}><img className="lg:w-6 w-5" src={assets.basket_icon} alt="" /></Link>
               <div className={getTotalCartAmount()===0?"":"dot absolute min-h-[10px] min-w-[10px] bg-[#ff6347] rounded-full -top-2 -right-2"}></div>
             </div>
-            <button onClick={()=>setShowLogin(true)} className="text-base text-[#ff6347] hover:text-white border border-[#ff6347] lg:py-2 py-1.5 lg:px-6 px-4 rounded-full cursor-pointer transition-colors duration-300 ease-linear hover:bg-[#ff6347]">
+            {!token?<button onClick={()=>setShowLogin(true)} className="text-base text-[#ff6347] hover:text-white border border-[#ff6347] lg:py-2 py-1.5 lg:px-6 px-4 rounded-full cursor-pointer transition-colors duration-300 ease-linear hover:bg-[#ff6347]">
               Sign-In
             </button>
+            :<div className="relative group">
+              <img src={assets.profile_icon} alt="" />
+              <ul className="absolute hidden min-w-36 right-0 z-10 group-hover:flex group-hover:flex-col group-hover:gap-2.5 group-hover:bg-[#fff2ef] group-hover:py-3 group-hover:px-6 group-hover:rounded group-hover:border-[1px_solid_tomato] group-hover:outline-[2px_solid_white] group-hover:list-none">
+                <li className="flex items-center gap-2.5 cursor-pointer"><img className="w-5" src={assets.bag_icon} alt="" /><p className="hover:text-[tomato]">Orders</p></li><hr />
+                <li onClick={logout} className="flex items-center gap-2.5 cursor-pointer"><img className="w-5" src={assets.logout_icon} alt="" /><p className="hover:text-[tomato]">Log Out</p></li>
+              </ul>
+              </div>}
+            
           </div>
         </div>
       </div>
