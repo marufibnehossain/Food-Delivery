9a8260d56484f139a1835b468398254fa5d1cb5b
Maruf <marufibnhossain@gmail.com>
2024-09-22 22:32:06 +0600

    completed

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ import orderRouter from "./routes/orderRoute.js";
 // dotenv.config()
 // app config
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 4000
 
 // middleware
 app.use(express.json())
